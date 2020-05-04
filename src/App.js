e12cfb75b96f18cc1e51670c6d7cd7b45613cca8
Suraj Tamang <surajmoktan40@gmail.com>
2020-05-04 12:41:16 -0400

    first app intiated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    Hi here is me!!
+    this is for test
+    </div>
+  );
+}
+
+export default App;
